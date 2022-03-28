4371a71418f6b74b9cd9d91c20e927d787d1ac61
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-28 20:33:11 +0200

    new: Role filter

---
+++ src/main/java/uma/taw/ubay/filter/RoleFilter.java
@@ -0,0 +1,46 @@
+package uma.taw.ubay.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import uma.taw.ubay.SessionKeys;
+import uma.taw.ubay.entity.KindEnum;
+import uma.taw.ubay.entity.LoginCredentialsEntity;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RoleFilter extends HttpFilter {
+
+    private List<KindEnum> role;
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        String roleParameter = config.getInitParameter("role");
+        if (roleParameter == null) throw new ServletException("RoleFilter requires a `role` parameter");
+
+        role = Arrays.stream(roleParameter.split("[;]"))
+                .map(KindEnum::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = req.getSession();
+        LoginCredentialsEntity credentialsEntity = (LoginCredentialsEntity) session.getAttribute(SessionKeys.LOGIN_CREDENTIALS);
+        KindEnum sessionKind = credentialsEntity.getKind();
+
+        if (role.stream().anyMatch(x->x.equals(sessionKind))) {
+            chain.doFilter(req,res);
+        } else {
+            // 401 - Unauthorised
+            res.sendError(401, "The current user is not an administrator");
+        }
+    }
+}
