4d4d85623b51272b600dbadf048ecec0ff0ffe9d
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-04-28 19:45:05 +0200

    fix: Redirect to /products instead of 404 page

--- src/main/java/uma/taw/ubay/servlet/Index.java
+++ src/main/java/uma/taw/ubay/servlet/Index.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/")
+@WebServlet("/index.jsp")
 public class Index extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
