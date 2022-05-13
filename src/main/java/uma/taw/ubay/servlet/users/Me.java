39ea44fa73837b89d7a306bbfda9079af3c55e76
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-05-13 18:52:02 +0200

    fix: #58

--- src/main/java/uma/taw/ubay/servlet/users/Me.java
+++ src/main/java/uma/taw/ubay/servlet/users/Me.java
@@ -1,7 +1,6 @@
 package uma.taw.ubay.servlet.users;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,7 +12,7 @@ import java.io.IOException;
  */
 
 //Not in use.
-@WebServlet("/users/me")
+//@WebServlet("/users/me")
 public class Me extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
