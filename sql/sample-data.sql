202583c8afe9e77c427fb92516813006d631e0e2
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-05-15 15:24:38 +0200

    new: Sample data for keynote

---
+++ sql/sample-data.sql
@@ -0,0 +1,21 @@
+insert into category (name, description) values ('Juegos', 'Pictionary, Monopoly... Juegos clásicos!!');
+insert into category (name, description) values ('Jardín y piscina', 'Disfruta del verano con estas ofertas');
+insert into category (name, description) values ('Educación', 'La vuelta al cole con Ubay');
+insert into category (name, description) values ('Motor', 'Los mejores coches y motos');
+insert into category (name, description) values ('Música', 'Ahora también en Streaming');
+
+
+insert into client (name, last_name, address, city, birth_date, gender) values ('Linet', 'Yeskin', '7 Talmadge Pass', 'Buckland', '2022-02-19', 'other');
+insert into client (name, last_name, address, city, birth_date, gender) values ('Dorelle', 'Moores', '5 Lakeland Circle', 'Quba', '2021-08-23', 'male');
+insert into client (name, last_name, address, city, birth_date, gender) values ('Brewer', 'Robel', '6442 Cody Lane', 'Youxi Chengguanzhen', '2021-07-26', 'male');
+insert into client (name, last_name, address, city, birth_date, gender) values ('Sapphira', 'Iddens', '9660 Norway Maple Lane', 'Wawrzeńczyce', '2021-04-15', 'male');
+insert into client (name, last_name, address, city, birth_date, gender) values ('Sonny', 'Ney', '9353 Roth Place', 'Hane', '2021-05-03', 'other');
+
+insert into login_credentials values ('user1','$2a$10$dLIkjXzk4L9q8YQ8cudI4O.44OO2ii5Cu9F00AtjHx.31lP9TZpAG','client',1);
+insert into login_credentials values ('user2','$2a$10$dLIkjXzk4L9q8YQ8cudI4O.44OO2ii5Cu9F00AtjHx.31lP9TZpAG','client',2);
+insert into login_credentials values ('user3','$2a$10$dLIkjXzk4L9q8YQ8cudI4O.44OO2ii5Cu9F00AtjHx.31lP9TZpAG','client',3);
+insert into login_credentials values ('user4','$2a$10$dLIkjXzk4L9q8YQ8cudI4O.44OO2ii5Cu9F00AtjHx.31lP9TZpAG','client',4);
+insert into login_credentials values ('user5','$2a$10$dLIkjXzk4L9q8YQ8cudI4O.44OO2ii5Cu9F00AtjHx.31lP9TZpAG','client',5);
+
+insert into login_credentials values ('admin','$2a$10$dLIkjXzk4L9q8YQ8cudI4O.44OO2ii5Cu9F00AtjHx.31lP9TZpAG','admin',null)
+
